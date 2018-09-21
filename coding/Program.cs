e73b0e1b43859ed04f04ab4f890212043beab24b
abler <mail@xiechaoyi.com>
2018-09-21 11:40:33 +0800

    Signed-off-by: abler <mail@xiechaoyi.com>

---
+++ coding/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace server
+{
+    public class Program
+    {
+        public static string index = "index.html";
+        public static string root = Directory.GetCurrentDirectory() + "/wwwroot/";
+        public static byte[] Html = System.Text.UTF8Encoding.UTF8.GetBytes(errorHtml);
+        public const string errorHtml = "<html><head><title>Server Error</title><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width,initial-scale=1,user-scalable=0\"/></head><body onselectstart=\"return false;\" style=\"text-align:center;background:#f9f9f9;\">not found index.html</body></html>";
+        public static void LoadHtml()
+        {
+            if (System.IO.File.Exists(root + "index.html"))
+            {
+                index = "index.html";
+            }
+            else if (System.IO.File.Exists(root + "index.htm"))
+            {
+                index = "index.htm";
+            }
+            else if (System.IO.File.Exists(root + "default.htm"))
+            {
+                index = "default.htm";
+            }
+            else if (System.IO.File.Exists(root + "default.html"))
+            {
+                index = "default.html";
+            }
+            else if (System.IO.File.Exists(root + "welcome.html"))
+            {
+                index = "welcome.html";
+            }
+
+            if (System.IO.File.Exists(root + index))
+            {
+                Html = System.IO.File.ReadAllBytes(root + index);
+            }
+            else
+            {
+                Html = System.Text.UTF8Encoding.UTF8.GetBytes(errorHtml);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            LoadHtml();
+            try
+            {
+                IDisposable regiser = null;
+                Action<object> callback = null;
+                var fileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(root);
+                callback = _ =>
+                {
+                    if (regiser != null)
+                    {
+                        regiser.Dispose();
+                    }
+                    System.Threading.Tasks.Task.Delay(100).Wait();
+                    LoadHtml();
+                    regiser = fileProvider.Watch(index).RegisterChangeCallback(callback, null);
+                };
+                regiser = fileProvider.Watch(index).RegisterChangeCallback(callback, null);
+            }
+            catch
+            {
+                Console.WriteLine("Watch file change is error!");
+            }
+
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseStartup<Startup>()
+            .UseKestrel(o =>
+            {
+                o.Listen(System.Net.IPAddress.Any, 80);
+            })
+            .Build();
+            host.Run();
+        }
+    }
+}
---
+++ coding/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.Run(async (context) =>
+            {
+                var req = context.Request.Path.Value;
+                if (req == "/")
+                {
+                    context.Response.ContentType = "text/html";
+                    context.Response.Body.Write(Program.Html, 0, Program.Html.Length);
+                }
+                else if (req != "/favicon.ico")
+                {
+                    context.Response.HttpContext.Abort();
+                }
+            });
+        }
+    }
+}
